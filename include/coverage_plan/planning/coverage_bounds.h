418e5a9d92dc33c7f0aba454245d00ebd4ddab70
charlie1329 <me@charliestreet.net>
2023-08-18 12:48:06 +0100

    Written bound classes for coverage planner

---
+++ include/coverage_plan/planning/coverage_bounds.h
@@ -0,0 +1,119 @@
+/**
+ * Header file for custom bounds for coverage planning.
+ *
+ * @author Charlie Street
+ */
+
+#ifndef COVERAGE_BOUNDS_H
+#define COVERAGE_BOUNDS_H
+
+#include "coverage_plan/mod/imac.h"
+#include "coverage_plan/planning/action.h"
+#include <Eigen/Dense>
+#include <despot/core/globals.h>
+#include <despot/core/history.h>
+#include <despot/interface/default_policy.h>
+#include <despot/interface/lower_bound.h>
+#include <despot/interface/pomdp.h>
+#include <despot/interface/upper_bound.h>
+#include <despot/random_streams.h>
+#include <memory>
+#include <vector>
+
+/**
+ * An upper bound which returns the max cells that can still be covered.
+ * This looks like min(num_cells-state.visited.size(),time_bound-state.time)
+ * Attributes:
+ *  _numCells: The number of cells in the map
+ * _timeBound: The max time bound on planning
+ */
+class MaxCellsUpperBound : despot::ParticleUpperBound {
+private:
+  const int _numCells{};
+  const int _timeBound{};
+
+public:
+  /**
+   * Constructor initialises attributes.
+   *
+   * @param numCells The number of cells in the map
+   * @param timeBound The max time bound on planning
+   */
+  MaxCellsUpperBound(const int &numCells, const int &timeBound)
+      : ParticleUpperBound{}, _numCells{numCells}, _timeBound{timeBound} {}
+
+  /**
+   * Returns an upper bound on the max reward obtainable from state.
+   *
+   * Note the coverage planning problem is finite, with no discount factor.
+   *
+   * @param state The state to evaluate the reward from
+   * @return maxCumulativeReward The max cumulative reward obtainable from state
+   */
+  double Value(const despot::State &state) const;
+};
+
+/**
+ * A very simple lower bound which just returns zero.
+ * This is part of my effort to enforce a finite horizon within despot.
+ */
+class ZeroParticleLowerBound : despot::ParticleLowerBound {
+public:
+  /**
+   * Initialise model to nullptr as not used.
+   */
+  ZeroParticleLowerBound() : ParticleLowerBound(nullptr) {}
+
+  /**
+   * The action shouldn't matter here, just returning 0.0 as horizon reached.
+   *
+   * @param particles A vector of states
+   *
+   * @return valuedAction The corresponding action and value (0.0)
+   */
+  despot::ValuedAction
+  Value(const std::vector<despot::State *> &particles) const;
+};
+
+/**
+ * A default policy which greedily chooses action based on immediate reward.
+ *
+ * Attributes:
+ * As in superclass, plus:
+ * _ imacEntry: The IMac entry matrix
+ * _imacExit: The IMac exit matrix
+ *
+ */
+class GreedyCoverageDefaultPolicy : despot::DefaultPolicy {
+
+private:
+  const Eigen::MatrixXd _imacEntry{};
+  const Eigen::MatrixXd _imacExit{};
+
+public:
+  /**
+   * Constructor initialises attributes.
+   *
+   * @param model The POMDP
+   * @param particleLowerBound A lower bound on the cumulative reward
+   * @param imac An IMac instance
+   */
+  GreedyCoverageDefaultPolicy(const despot::DSPOMDP *model,
+                              despot::ParticleLowerBound *particleLowerBound,
+                              std::shared_ptr<IMac> imac)
+      : DefaultPolicy{model, particleLowerBound},
+        _imacEntry{imac->getEntryMatrix()}, _imacExit{imac->getExitMatrix()} {}
+
+  /**
+   * Function greedily chooses an action weighted on the particles.
+   *
+   * @param particles The states at the head (latest?) of the scenarios
+   * @param streams Random streams attached to the scenarios
+   * @param history The current action-observation history
+   */
+  despot::ACT_TYPE Action(const std::vector<despot::State *> &particles,
+                          despot::RandomStreams &streams,
+                          despot::History &history) const;
+};
+
+#endif
---
+++ src/planning/coverage_bounds.cpp
@@ -0,0 +1,69 @@
+/**
+ * Implementation of the bounds in coverage_bounds.h.
+ * @see coverage_bounds.h
+ *
+ * @author Charlie Street
+ */
+
+#include "coverage_plan/planning/coverage_bounds.h"
+#include "coverage_plan/planning/action.h"
+#include "coverage_plan/planning/coverage_state.h"
+#include <algorithm>
+#include <despot/core/globals.h>
+#include <despot/interface/default_policy.h>
+#include <iterator>
+
+/**
+ * Returns an upper bound on the max reward obtainable from state.
+ */
+double MaxCellsUpperBound::Value(const despot::State &state) const {
+  const CoverageState &coverState{static_cast<const CoverageState &>(state)};
+  return std::min((double)(this->_numCells - coverState.covered.size()),
+                  (double)(this->_timeBound - coverState.time));
+}
+
+/**
+ * The action shouldn't matter here, just returning 0.0 as horizon reached.
+ */
+despot::ValuedAction ZeroParticleLowerBound::Value(
+    const std::vector<despot::State *> &particles) const {
+  return despot::ValuedAction(ActionHelpers::toInt(Action::up), 0.0);
+}
+
+/**
+ * Function greedily chooses an action weighted on the particles.
+ *
+ * @param particles The states at the head (latest?) of the scenarios
+ * @param streams Random streams attached to the scenarios
+ * @param history The current action-observation history
+ */
+despot::ACT_TYPE GreedyCoverageDefaultPolicy::Action(
+    const std::vector<despot::State *> &particles,
+    despot::RandomStreams &streams, despot::History &history) const {
+  // Entry for each action
+  std::vector<double> immRewards{};
+  for (int i{0}; i < this->model_->NumActions(); ++i) {
+    immRewards.push_back(0);
+  }
+
+  for (despot::State *state : particles) { // Get weighted immediate reward
+    CoverageState *coverState{static_cast<CoverageState *>(state)};
+    for (int a{0}; a < this->model_->NumActions(); ++a) {
+      GridCell succLoc{ActionHelpers::applySuccessfulAction(
+          coverState->robotPosition, ActionHelpers::fromInt(a))};
+      if (coverState->covered.count(succLoc) != 0) { // If not already covered
+        // prob of being free in next step weighted by particle weight
+        if (coverState->map(succLoc.y, succLoc.x) == 1) { // occupied, use exit
+          immRewards.at(a) +=
+              (this->_imacExit(succLoc.y, succLoc.x) * coverState->weight);
+        } else { // free, use 1 - entry
+          immRewards.at(a) += ((1.0 - this->_imacEntry(succLoc.y, succLoc.x)) *
+                               coverState->weight);
+        }
+      }
+    }
+  }
+  // Get location of max element (max action)
+  return std::distance(immRewards.begin(),
+                       std::max_element(immRewards.begin(), immRewards.end()));
+}
