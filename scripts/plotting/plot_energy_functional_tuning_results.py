070bb43a740000ae7b7366dd6455a45da5cfed15
charlie1329 <me@charliestreet.net>
2023-10-09 15:01:48 +0100

    Test results for neighbour inclusion in getUncovered

--- scripts/plotting/plot_energy_functional_tuning_results.py
+++ scripts/plotting/plot_energy_functional_tuning_results.py
@@ -20,17 +20,17 @@ def collect_results(results_files):
     """Read in the results from the files.
 
     Args:
-        results_files: wall points flag to file path
+        results_files: method to file path
 
     Returns:
-        results: wall points flag to env to results list
+        results: method to env to results list
     """
     results = {}
 
-    for wall_points_flag in results_files:
+    for method in results_files:
         results_for_val = {}
 
-        with open(results_files[wall_points_flag], "r") as csv_file:
+        with open(results_files[method], "r") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=",")
             for row in csv_reader:  # Row is env, results
                 res = list(map(lambda x: float(x), row[1:-1]))
@@ -39,7 +39,7 @@ def collect_results(results_files):
                     if i % 2 == 1:
                         results_for_val[row[0]].append(res[i])
 
-        results[wall_points_flag] = results_for_val
+        results[method] = results_for_val
 
     return results
 
@@ -60,25 +60,32 @@ def set_box_colors(bp):
     plt.setp(bp["whiskers"][3], color="tab:red", linewidth=8.0)
     plt.setp(bp["medians"][1], color="tab:red", linewidth=8.0)
 
+    plt.setp(bp["boxes"][2], color="tab:green", linewidth=8.0)
+    plt.setp(bp["caps"][4], color="tab:green", linewidth=8.0)
+    plt.setp(bp["caps"][5], color="tab:green", linewidth=8.0)
+    plt.setp(bp["whiskers"][4], color="tab:green", linewidth=8.0)
+    plt.setp(bp["whiskers"][5], color="tab:green", linewidth=8.0)
+    plt.setp(bp["medians"][2], color="tab:green", linewidth=8.0)
+
 
 def plot_results(results, env):
     """Plot the results for a given environment.
 
     Args:
-        results: wall_points flag to env to results list
+        results: method to env to results list
         env: The environment name to plot
     """
     print("Plotting results")
 
     results_list = []
-    wall_points_vals = [False, True]
-    for wall_points_flag in wall_points_vals:
-        results_list.append(results[wall_points_flag][env])
+    methods = ["no_wall_points", "with_wall_points", "neighbours_get_uncovered"]
+    for method in methods:
+        results_list.append(results[method][env])
 
     box = plt.boxplot(
         results_list,
         whis=[0, 100],
-        positions=[1, 2],
+        positions=[1, 2, 3],
         widths=0.6,
     )
     set_box_colors(box)
@@ -93,7 +100,10 @@ def plot_results(results, env):
     )
     plt.ylabel("Proportion Covered")
 
-    plt.xticks([1, 2], ["No Wall Points Term", "With Wall Points Term"])
+    plt.xticks(
+        [1, 2, 3],
+        ["No Wall Points Term", "With Wall Points Term", "Neighbours in getUncovered"],
+    )
 
     plt.xlabel("Parameters", fontsize=40)
     plt.show()
@@ -103,22 +113,22 @@ def plot_combined_results(results):
     """Plot the results combined across all environments.
 
     Args:
-        results: wall_points_val to env to results list
+        results: method to env to results list
     """
     print("Plotting results")
 
     results_list = []
-    wall_points_vals = [False, True]
-    for wall_points_flag in wall_points_vals:
+    methods = ["no_wall_points", "with_wall_points", "neighbours_get_uncovered"]
+    for method in methods:
         combined_results = []
-        for env in results[wall_points_flag]:
-            combined_results += results[wall_points_flag][env]
+        for env in results[method]:
+            combined_results += results[method][env]
         results_list.append(combined_results)
 
     box = plt.boxplot(
         results_list,
         whis=[0, 100],
-        positions=[1, 2],
+        positions=[1, 2, 3],
         widths=0.6,
     )
     set_box_colors(box)
@@ -132,7 +142,10 @@ def plot_combined_results(results):
         labelsize=28,
     )
     plt.ylabel("Proportion Covered")
-    plt.xticks([1, 2], ["No Wall Points Term", "With Wall Points Term"])
+    plt.xticks(
+        [1, 2, 3],
+        ["No Wall Points Term", "With Wall Points Term", "Neighbours in getUncovered"],
+    )
     plt.xlabel("Parameters", fontsize=40)
     plt.show()
 
@@ -141,15 +154,19 @@ def plot_statistics(results):
     """Plot the means and medians over the combined results.
 
     Args:
-        results: num_scenarios to env to results list
+        results: method to env to results list
     """
-    for wall_points_flag in [False, True]:
+    for method in [
+        "no_wall_points",
+        "with_wall_points",
+        "neighbours_get_uncovered",
+    ]:
         results_for_params = []
-        for env in results[wall_points_flag]:
-            results_for_params += results[wall_points_flag][env]
+        for env in results[method]:
+            results_for_params += results[method][env]
         print(
-            "WALL POINTS TERM: {}, MEAN: {}, MEDIAN: {}".format(
-                wall_points_flag,
+            "METHOD: {}, MEAN: {}, MEDIAN: {}".format(
+                method,
                 np.mean(results_for_params),
                 np.median(results_for_params),
             )
@@ -164,27 +181,27 @@ def plot_stat_sig(results):
     TODO: Check its mannwhitneyu, not paired samples t-test
 
     Args:
-        results: wall_points_flag to env to results list
+        results: method to env to results list
     """
     combined = {}
-    wall_points_vals = [False, True]
-    for wall_points_flag in wall_points_vals:
+    methods = ["no_wall_points", "with_wall_points", "neighbours_get_uncovered"]
+    for method in methods:
         results_for_params = []
-        for env in results[wall_points_flag]:
-            results_for_params += results[wall_points_flag][env]
-            combined[wall_points_flag] = results_for_params
+        for env in results[method]:
+            results_for_params += results[method][env]
+            combined[method] = results_for_params
 
-    print("BEST = True")
+    print("BEST = with_wall_points")
 
-    for wall_points_flag in wall_points_vals:
+    for method in methods:
         p = mannwhitneyu(
-            combined[True],
-            combined[wall_points_flag],
+            combined["with_wall_points"],
+            combined[method],
             alternative="greater",
         )[1]
         print(
-            "True > {}: p = {}, stat sig better = {}".format(
-                wall_points_flag, p, p < 0.05
+            "with_wall_points > {}: p = {}, stat sig better = {}".format(
+                method, p, p < 0.05
             )
         )
 
@@ -193,16 +210,19 @@ if __name__ == "__main__":
     results_dir = os.path.abspath("../../data/results/energy_functional_tuning")
 
     results_files = {}
-    results_files[False] = os.path.join(
+    results_files["no_wall_points"] = os.path.join(
         results_dir, "energy_functional_no_wall_points_term_results.csv"
     )
-    results_files[True] = os.path.join(
+    results_files["with_wall_points"] = os.path.join(
         results_dir, "energy_functional_with_wall_points_term_results.csv"
+    )
+    results_files["neighbours_get_uncovered"] = os.path.join(
+        results_dir, "energy_functional_neighbours_in_get_uncovered_results.csv"
     )
 
     results = collect_results(results_files)
 
-    plot_results(results, "four_light")
-    plot_combined_results(results)
-    # plot_statistics(results)
-    # plot_stat_sig(results)
+    # plot_results(results, "four_light")
+    # plot_combined_results(results)
+    plot_statistics(results)
+    plot_stat_sig(results)
