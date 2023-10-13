84063c46f0daa4ed97c2afb1ca5453b78d821435
charlie1329 <me@charliestreet.net>
2023-10-13 09:43:01 +0100

    First lifelong test. Results looking odd.

--- scripts/plotting/plot_multi_episode_test_results.py
+++ scripts/plotting/plot_multi_episode_test_results.py
@@ -22,44 +22,42 @@ def plot_results(results_file):
     """Plot the results file with std around it.
 
     Args:
-        results_file: The csv file with all the results
+        results: The csv file with all the results
     """
-    runs = []
     with open(results_file, "r") as csv_in:
         csv_reader = csv.reader(csv_in, delimiter=",")
 
         for row in csv_reader:
-            runs.append([float(x) for x in row])
+            results = [float(x) for x in row[:-1]]
 
     # Format into each episode's results over the multiple repeats
-    runs_formatted = []
-    for i in range(200):
-        day_res = []
-        for run in runs:
-            day_res.append(run[i])
-
-        runs_formatted.append(day_res)
+    # runs_formatted = []
+    # for i in range(200):
+    #    day_res = []
+    #    for run in runs:
+    #        day_res.append(run[i])
+    #    runs_formatted.append(day_res)
 
     # x axis of our plot
-    episodes = list(range(1, 201))
+    episodes = list(range(1, 301))
 
     # main line of our plot
-    means = [np.mean(r) for r in runs_formatted]
-    plt.plot(episodes, means, linewidth=3.0)
+    # means = [np.mean(r) for r in runs_formatted]
+    plt.plot(episodes, results, linewidth=3.0)
 
     # Show the std as well
-    std = [np.std(r) for r in runs_formatted]
-    mean_plus = [means[i] + std[i] for i in range(len(means))]
-    mean_minus = [means[i] - std[i] for i in range(len(means))]
-    plt.fill_between(episodes, mean_plus, mean_minus, alpha=0.5)
+    # std = [np.std(r) for r in runs_formatted]
+    # mean_plus = [means[i] + std[i] for i in range(len(means))]
+    # mean_minus = [means[i] - std[i] for i in range(len(means))]
+    # plt.fill_between(episodes, mean_plus, mean_minus, alpha=0.5)
 
     plt.xlabel("Episode")
     plt.ylabel("Proportion Covered")
-    plt.xlim((1, 201))
+    plt.xlim((1, 301))
     plt.show()
 
 
 if __name__ == "__main__":
     plot_results(
-        "/home/charlie/work/coverage-plan/data/results/" + "MultiEpisodeTestResults.csv"
+        "/home/charlie/work/coverage-plan/data/results/prelim_exps/lifelong_test/maximum_likelihood_results.csv"
     )
