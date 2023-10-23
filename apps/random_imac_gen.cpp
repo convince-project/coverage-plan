0464e53fb86c017eb823eb386ba2d053b00a20f1
charlie1329 <me@charliestreet.net>
2023-10-23 15:01:53 +0100

    New, more intense dynamics filled environments

--- apps/random_imac_gen.cpp
+++ apps/random_imac_gen.cpp
@@ -16,6 +16,78 @@
 #include <random>
 
 /**
+ * Create a random IMac instance with jmore complex dynamics
+ *
+ * @param xDim X dimension of the map
+ * @param yDim Y dimension of the map
+ * @param propSlow Proportion of the map to have slow dynamics (0.05)
+ * @param propMed Proportion of the map to have medium dynamics (0.2)
+ * @param propFast Proportion of the map to have fast dynamics (0.1)
+ *
+ * 1-(propSlow+propMed+propFast) of the map is free
+ *
+ * @return imac A shared pointer to an IMac instance
+ */
+std::shared_ptr<IMac> createMoreComplexIMac(const int &xDim, const int &yDim,
+                                            const double &propSlow,
+                                            const double &propMed,
+                                            const double &propFast) {
+  Eigen::MatrixXd entryMatrix{yDim, xDim};
+  Eigen::MatrixXd exitMatrix{yDim, xDim};
+  Eigen::MatrixXd initialBelief{yDim, xDim};
+
+  int numSet{0};
+  const int slowLimit{(int)ceil(propSlow * (xDim * yDim))};
+  const int medLimit{(int)ceil(slowLimit + (propMed * (xDim * yDim)))};
+  const int fastLimit{(int)ceil(medLimit + (propFast * (xDim * yDim)))};
+
+  std::cout << "(Free, Fast, Med, Slow): (" << ((xDim * yDim) - fastLimit)
+            << ", " << fastLimit - medLimit << ", " << medLimit - slowLimit
+            << ", " << slowLimit << ")\n";
+
+  // Random order of cells on grid map
+  std::vector<GridCell> cells{};
+  for (int x{0}; x < xDim; ++x) {
+    for (int y{0}; y < yDim; ++y) {
+      cells.push_back(GridCell{x, y});
+    }
+  }
+  std::mt19937_64 rng{SeedHelpers::genRandomDeviceSeed()};
+  std::shuffle(std::begin(cells), std::end(cells), rng);
+
+  std::uniform_real_distribution<> perturb{-0.01, 0.01};
+
+  // Have to use y,x to match coordinate systems up
+  // Add some perturbations to the dynamic cells to make it more representative
+  for (const GridCell &cell : cells) {
+    if (numSet < slowLimit) {
+      entryMatrix(cell.y, cell.x) = 0.05 + perturb(rng);
+      exitMatrix(cell.y, cell.x) = 0.05 + perturb(rng);
+      initialBelief(cell.y, cell.x) = 0.5 + perturb(rng);
+    } else if (numSet < medLimit) {
+      entryMatrix(cell.y, cell.x) = 0.2 + perturb(rng);
+      exitMatrix(cell.y, cell.x) = 0.2 + perturb(rng);
+      initialBelief(cell.y, cell.x) = 0.5 + perturb(rng);
+    } else if (numSet < fastLimit) {
+      entryMatrix(cell.y, cell.x) = 0.5 + perturb(rng);
+      exitMatrix(cell.y, cell.x) = 0.5 + perturb(rng);
+      initialBelief(cell.y, cell.x) = 0.5 + perturb(rng);
+    } else { // static free
+      entryMatrix(cell.y, cell.x) = 0.0;
+      exitMatrix(cell.y, cell.x) = 1.0;
+      initialBelief(cell.y, cell.x) = 0.0;
+    }
+    ++numSet;
+
+    if (cell == GridCell{0, 0}) { // Ensure robot init position is free
+      initialBelief(cell.y, cell.x) = 0.0;
+    }
+  }
+
+  return std::make_shared<IMac>(entryMatrix, exitMatrix, initialBelief);
+}
+
+/**
  * Create a random IMac instance of a given size and dynamics.
  *
  * @param xDim X dimension of the map
@@ -105,6 +177,24 @@ void sampleRuns(std::shared_ptr<IMac> imac, const std::filesystem::path &dir,
 }
 
 int main() {
+
+  // Creating two environments with more intense dynamics
+  // Five by five with 20,30,10,40 (slow, med, fast, static)
+  std::cout << "5x5 with 20 slow, 30 med, 10 fast, 40 slow\n";
+  std::shared_ptr<IMac> fiveVeryHeavy{
+      createMoreComplexIMac(5, 5, 0.2, 0.3, 0.1)};
+  std::string fiveVeryHeavyDir{"../../data/prelim_exps/five_very_heavy"};
+  fiveVeryHeavy->writeIMac(fiveVeryHeavyDir);
+  sampleRuns(fiveVeryHeavy, fiveVeryHeavyDir, 33, 40);
+
+  // Seven by seven with 20,30,10,40 (slow, med, fast, static)
+  std::cout << "7x7 with 20 slow, 30 med, 10 fast, 40 slow\n";
+  std::shared_ptr<IMac> sevenVeryHeavy{
+      createMoreComplexIMac(7, 7, 0.2, 0.3, 0.1)};
+  std::string sevenVeryHeavyDir{"../../data/prelim_exps/seven_very_heavy"};
+  sevenVeryHeavy->writeIMac(sevenVeryHeavyDir);
+  sampleRuns(sevenVeryHeavy, sevenVeryHeavyDir, 64, 40);
+  exit(1);
 
   // Five by five w/ lots of semi-static obstacles
   std::cout << "Five by five w/ lots of semi-static s\n";
